9363e73c68e09d28d42b955f10e98d5e96fd1ba7
David Konsumer <konsumer@jetboystudio.com>
2023-05-01 11:00:01 -0700

    imrpoved auth handling

--- mitm_google_fdfe.py
+++ mitm_google_fdfe.py
@@ -9,44 +9,91 @@ outDir = "/out"
 import time
 import json
 import logging
+import urllib.parse
 
+# get headers object from request
 def getHeaders(request):
   headers = {}
   for k,v in request.headers.items():
     headers[k] = v
   return headers
 
-
+# handle all responses
 def response(flow):
   if flow.request.path.startswith("/auth"):
-    headers = getHeaders(flow.request)
-    op = "auth"
-    fname = time.strftime(f"%Y-%m-%d_%H-%M-%S_{op}")
-    
-    logging.info(f"Saved {fname}.mjs in {outDir}")
-    body = flow.response.get_content()
-    auth = {}
-    for line in body.split("\n"):
-      k,v = line.split("=")
-      auth[k] = v
-
     url = flow.request.scheme + "://" + flow.request.pretty_host + flow.request.path
+    outS = flow.request.get_text()
+    if outS:
+      out = urllib.parse.parse_qs(outS)
+      if out["app"] and out["app"][0] == "com.android.vending":
+        op = "auth"
+        fname = time.strftime(f"%Y-%m-%d_%H-%M-%S_{op}")
+        auth_refersh = {}
+        auth_refersh["email"] = out["Email"][0]
+        auth_refersh["token"] = out["Token"][0]
+        auth_refersh["gsfid"] = out["androidId"][0]
+        auth_refersh["language"] = out["lang"][0]
+        auth_refersh["country"] = out["device_country"][0]
+        auth_refersh["google_play_services_version"] = out["google_play_services_version"][0]
+        auth_refersh["sdk_version"] = out["sdk_version"][0]
+        auth_refersh["signature"] = out["client_sig"][0]
 
-    jf = open(f"{outDir}/{fname}.js", "w")
-    jf.write("// headers that were sent\nexport const headers = " + json.dumps(headers, indent = 2) + "\n\n")
-    jf.write("// body that was sent\nexport const body = " +  json.dumps(hflow.request.get_text()) + "\n")
-    jf.write("// AUTH call\nexport const request = () => fetch('" + url + "', { method: '" + flow.request.method + "', headers, body })\n\n")
-    jf.write("export default request\n")
-    jf.close()
+        jr = open(f"{outDir}/{fname}-refresh.json", "w")
+        jr.write(json.dumps(auth_refersh, indent = 2))
+        jr.close()
 
-    jb = open(f"{outDir}/{fname}.json", "wb")
-    jb.write(json.dumps(auth, indent = 2))
-    jb.close()
+        params = {}
+        for k in out:
+          params[k] = out[k][0]
+       
+        headers = getHeaders(flow.request)
+        jf = open(f"{outDir}/{fname}.js", "w")
+        jf.write("// headers that were sent\nexport const headers = " + json.dumps(headers, indent = 2) + "\n\n")
+        jf.write("// body that was sent\nexport const params = " + json.dumps(params, indent = 2) + "\n\n")
+        jf.write("// AUTH call\nexport const request = () => fetch('" + url + "', { method: 'POST', body: new URLSearchParams(params), headers })\n\n")
+        jf.write("export default request\n")
+        jf.close()
+
+        body = flow.response.get_text()
+
+        if body:
+          jo = open(f"{outDir}/{fname}-out.txt", "w")
+          jo.write(body)
+          jo.close()
+
+          bodyD = {}
+          for line in body.split("\n"):
+            k,v = line.split("=")
+            bodyD[k]=v
+
+          auth_session = {}
+          auth_session["email"] = auth_refersh["email"]
+          auth_session["gsfid"] = auth_refersh["gsfid"]
+          auth_session["language"] = auth_refersh["language"]
+          auth_session["country"] = auth_refersh["country"]
+          auth_session["google_play_services_version"] = auth_refersh["google_play_services_version"]
+          auth_session["sdk_version"] = auth_refersh["sdk_version"]
+          auth_session["token"] = bodyD["it"]
+          auth_session["issueAdvice"] = bodyD["issueAdvice"]
+          auth_session["expiration"] = bodyD["Expiry"]
+          auth_session["expirationDuration"] = bodyD["ExpiresInDurationSec"]
+          auth_session["storeConsentRemotely"] = bodyD["storeConsentRemotely"]
+          auth_session["isTokenSnowballed"] = bodyD["isTokenSnowballed"]
+          auth_session["itMetadata"] = bodyD["itMetadata"]
+
+
+          js = open(f"{outDir}/{fname}-session.json", "w")
+          js.write(json.dumps(auth_session, indent = 2))
+          js.close()
+
+
+      else:
+        logging.info(f"Saw non-store auth: {url}")
 
 
   if flow.request.path.startswith("/fdfe"):
     headers = getHeaders(flow.request)
-    op = flow.request.path.split('?')[0].replace("/fdfe/", "").replace("'", "\\'")
+    op = flow.request.path.split('?')[0].replace("/fdfe/", "").replace("'", "\\'").replace('/', '-')
     fname = time.strftime(f"%Y-%m-%d_%H-%M-%S_{op}")
 
     logging.info(f"Saved {fname}.mjs and {fname}.pb in {outDir}")
